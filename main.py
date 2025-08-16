0adbb6ee899eab2c8d6ddcd3e9e5a2da801cfc4a
CoolCream <mahasankarshant@gmail.com>
2025-08-16 21:26:12 +0530

    added files

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import pygame, sys
+from game import Game
 
 pygame.init()
-darkBLue = (44, 44, 127)
+darkBlue = (44, 44, 127)
 
-screen = pygame.display.set_mode((300, 600))
+screen = pygame.display.set_mode((500, 620))
 pygame.display.set_caption("Tetris")
 
 clock = pygame.time.Clock()
+
+game = Game()
+
+GAME_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(GAME_UPDATE, 300)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if game.game_over == True:
+                game.game_over = False
+                game.reset()
+            if event.key == pygame.K_LEFT and game.game_over == False:
+                game.move_left()
+            if event.key == pygame.K_RIGHT and game.game_over == False:
+                game.move_right() 
+            if event.key == pygame.K_UP and game.game_over == False:
+                game.rotate() 
+        if event.type == GAME_UPDATE and game.game_over == False:
+            game.move_down()
+                
     
     #Drawing:
-    screen.fill(darkBLue)
+    screen.fill(darkBlue)
+    game.draw(screen)
+
     pygame.display.update()
     clock.tick(60)
